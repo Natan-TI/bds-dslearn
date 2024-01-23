1b42c6c445b0eb399b026a65c197821faaf6a788
NatanEasy <natan@easypoints.com.br>
2024-01-23 13:24:46 -0300

    Exceptions, validation, security

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dslearnbds.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.dslearnbds.entities.Deliver;
-
-public interface RoleRepository extends JpaRepository<Deliver, Long>{
-
-}
