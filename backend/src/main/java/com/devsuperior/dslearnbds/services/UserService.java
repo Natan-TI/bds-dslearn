945dbba4f2acf4f3f0a1800012a721e6bf9f5943
NatanEasy <natan@easypoints.com.br>
2024-01-23 15:20:35 -0300

    Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -25,13 +25,17 @@ public class UserService implements UserDetailsService {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private AuthService authService;
+	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		authService.validateSelfOrAdmin(id);
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
 		return new UserDTO(entity);
 	}
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = repository.findByEmail(username);
